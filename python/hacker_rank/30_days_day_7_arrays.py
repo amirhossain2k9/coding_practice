17d6d6de21450e05661e625fda9e9da6bdc43b4c
Amir Hossain <amirhossain2k9@gmail.com>
2019-03-11 12:24:24 +0600

    30 days challenge: day 7 -> arrays

---
+++ python/hacker_rank/30_days_day_7_arrays.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    arr.reverse()
+    array_string = ' '
+    arr = [str(i) for i in arr]
+    array_string = array_string.join(arr)
+
+    print(array_string)
+
