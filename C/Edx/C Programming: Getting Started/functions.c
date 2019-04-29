f4cca291bb5f42a4fd9ef91741f4ba21b203e2b4
Amir Hossain <amirhossain2k9@gmail.com>
2019-04-29 13:07:07 +0600

    C: functions definition

---
+++ C/Edx/C Programming: Getting Started/functions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+functions definitions:
+
+type function_name (type1 arg1, type2 arg2,....){
+    code statements..
+    ......
+    return value_of_type
+}
+*/
+
+void my_function(){
+    printf("\nsum=%d", 10+34);
+    printf("\ninside a function");
+}
+
+int main(){
+
+    my_function();
+    my_function();
+}
--- C/Edx/C Programming: Getting Started/hello world.c
+++ C/Edx/C Programming: Getting Started/hello world.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>  // 'inlcude'= preprocessor statement
 int main(void){
     printf("Hello from the Machine!");
     return 0;
