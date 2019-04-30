c1d1a0dcc2ba7dc9641f7bca2a179defea7dd52f
Amir Hossain <amirhossain2k9@gmail.com>
2019-04-30 16:31:07 +0600

    Python: 30 days challenge: day 8 -> dict

---
+++ python/hacker_rank/30_days_day_8_dict_maps.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+total_entries = int(input())
+
+phone_book = {}
+for entry in range(total_entries):
+    record_string = input().split(' ')
+    phone_book[record_string[0]] = record_string[1]
+
+queries = sys.stdin.readlines()
+for query in queries:
+    query_name = query.strip()
+    if query_name in phone_book:
+        print('{}={}'.format(query_name, phone_book[query_name]))
+    else:
+        print('Not found')
+
